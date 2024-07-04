5555d069d19bfa6c874551476d9db3eb0ae7c3c8
Arthur <arthur.cremelie@gmail.com>
2024-07-04 13:15:47 +0200

    add placeholder account information and awful logout button

--- src/screens/AccountScreen/index.tsx
+++ src/screens/AccountScreen/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
 	Animated,
 	SafeAreaView,
@@ -12,13 +13,23 @@ import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList, Routes } from '../../navigation/Routes';
 import { FocusAwareStatusBar } from '../../components/FocusAwareStatusBar';
 import { useAuth } from '../../providers/AuthProvider';
+import SectionHeader from '../../components/SectionHeader';
+import ProfileHeader from '../../components/ProfileHeader';
+import AccountHeader from '../../components/AccountHeader';
 
 type Props = {};
+
+/*
+--- TODO ---
+TODO: fix logout error with firebase permissions
+TODO: style user information (AccountHeader)
+TODO: style logout
+*/
 
 const AccountScreen = (props: Props) => {
 	const { navigate } =
 		useNavigation<NativeStackNavigationProp<RootStackParamList>>();
-	const { handleLogout } = useAuth();
+	const { handleLogout, user } = useAuth();
 
 	function logout(): void {
 		handleLogout();
@@ -28,18 +39,19 @@ const AccountScreen = (props: Props) => {
 		<SafeAreaView className='flex-1 items-center bg-dark-primaryBackground'>
 			<FocusAwareStatusBar translucent barStyle={'light-content'} />
 			<ScrollView
-				className='w-full mt-6'
+				className='w-full'
 				contentContainerStyle={{
 					alignItems: 'center',
 					justifyContent: 'center',
 				}}
 				showsVerticalScrollIndicator={false}>
-				<View className='w-full mt-6'>
-					<Text className='text-white font-inter-medium text-center'>
-						ACCOUNT
-					</Text>
-					<TouchableOpacity onPress={logout}>
-						<Text>Logout</Text>
+				<View className='w-full'>
+					<SectionHeader title='Your account' />
+					<AccountHeader name='Jan Janssens' imageUrl='https://i.pravatar.cc/300' />
+					<TouchableOpacity
+						className='bg-orange-600 py-3 px-6 rounded-lg mt-5'
+						onPress={logout}>
+						<Text className='text-white text-lg font-bold'>Logout</Text>
 					</TouchableOpacity>
 				</View>
 			</ScrollView>
