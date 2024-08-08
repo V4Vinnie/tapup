ffbde5dea5198eefab9b1fb65d42b7c4359a3ec1
Arthur <arthur.cremelie@gmail.com>
2024-08-08 21:09:52 +0200

    signup next button working again

--- src/screens/SignupScreen/index.tsx
+++ src/screens/SignupScreen/index.tsx
@@ -18,7 +18,6 @@ import AddCompanyCode from './AddCompanyCode';
 import AddInformation from './AddInformation';
 import { useAuth } from '../../providers/AuthProvider';
 import { useCompany } from '../../providers/CompanyProvider';
-import AppButton from '../../components/AppButton';
 
 type Props = {};
 
@@ -67,29 +66,41 @@ const SignupScreen = (props: Props) => {
 	}, [authErrors]);
 
 	return (
-		<SafeAreaView style={scrollViewContainer}>
-			<AppHeader headerWithBack title={'Signup'} />
-			<View className='flex-1 items-center justify-center bg-dark-primaryBackground'>
+		<SafeAreaView className='flex-1 bg-dark-primaryBackground'>
+			<AppHeader headerWithBack title='Sign Up' />
+			<KeyboardAwareScrollView
+				keyboardShouldPersistTaps={'handled'}
+			contentContainerStyle={scrollViewContentContainer}
+			showsVerticalScrollIndicator={false}
+			style={scrollViewContainer}>
+			<View className='flex-1 items-center bg-dark-primaryBackground'>
 				<FocusAwareStatusBar
 					translucent
 					backgroundColor='transparent'
 					barStyle='light-content'
 				/>
 
-				<View className='w-full h-4/5'>
+				<View className='h-full py-2 justify-between pb-8 pt-40'>
 					<Swiper
 						ref={swiper}
-						style={{ flex: 1 }}
-						showsPagination
-						paginationStyle={{
-							bottom: 10,
+						height={height / 1.5}
+						width={width}
+						containerStyle={{
+							marginBottom: isKeyboardOpen ? 30 : 80,
+							paddingBottom: isKeyboardOpen ? 20 : undefined,
 						}}
+						paginationStyle={{
+							bottom: authErrors ? -30 : 30,
+						}}
+						showsPagination
 						dot={<CustomSwiperDot />}
 						activeDot={<CustomSwiperDot active />}
 						scrollEnabled={false}
 						loop={false}
 						showsButtons={false}>
 						<RegisterUserDetails
+							//image={image}
+							//setImage={setImage}
 							username={username}
 							setProfilename={setProfilename}
 							email={email}
@@ -112,22 +123,25 @@ const SignupScreen = (props: Props) => {
 							isSending={isSending}
 						/>
 					</Swiper>
-				</View>
 
-				<View className='mt-4 flex-row items-center justify-center'>
-					<Text className='text-base font-inter-regular text-dark-subTextColor'>
-						{'Already have an account?'}
-					</Text>
-					<Pressable
-						onPress={() => {
-							navigate(Routes.LOGIN);
-						}}>
-						<Text className='text-base font-inter-bold text-dark-textColor'>
-							{' Login'}
-						</Text>
-					</Pressable>
+					{!isKeyboardOpen && (
+						<View className='flex-row items-center justify-center'>
+							<Text className='text-base font-inter-regular text-dark-subTextColor'>
+								{'Already have an account?'}
+							</Text>
+							<Pressable
+								onPress={() => {
+									navigate(Routes.LOGIN);
+								}}>
+								<Text className='text-base font-inter-bold text-dark-textColor'>
+									{' Login'}
+								</Text>
+							</Pressable>
+						</View>
+					)}
 				</View>
 			</View>
+			</KeyboardAwareScrollView>
 		</SafeAreaView>
 	);
 };
